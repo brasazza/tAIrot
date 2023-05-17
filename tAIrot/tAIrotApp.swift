51ae639c61fad4943c5e868215419553a66a0508
Brandon <contacto@senaamd.com>
2023-05-17 12:33:03 -0600

    Initial Commit

---
+++ tAIrot/tAIrotApp.swift
@@ -0,0 +1,17 @@
+//
+//  tAIrotApp.swift
+//  tAIrot
+//
+//  Created by Brandon Ramírez Casazza on 17/05/23.
+//
+
+import SwiftUI
+
+@main
+struct tAIrotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
