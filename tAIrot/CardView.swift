e7bed930417fa3935c08212d402b9db0586e7359
Brandon <contacto@senaamd.com>
2023-05-21 14:18:42 -0600

    Beta 2.3

--- tAIrot/CardView.swift
+++ tAIrot/CardView.swift
@@ -12,34 +12,75 @@ struct CardView: View {
     
     var body: some View {
         NavigationLink(destination: InputView(card: card)) {
-            VStack {
-                Text(card.title)
-                    .font(.largeTitle)
-                    .bold()
-                    .foregroundColor(.white)
-                    .padding()
+            ZStack {
+                CardBackgroundView(card: card)
                 
-                Text(card.description)
-                    .font(.title3)
-                    .foregroundColor(.white)
-                    .padding()
-                
-                Spacer()
-                
-                Text("Guess My Future")
-                    .font(.title2)
-                    .bold()
-                    .foregroundColor(.white)
-                    .padding()
-                    .background(Color.white.opacity(0.3))
-                    .cornerRadius(60)
+                VStack {
+                    Text(card.title)
+                        .font(.largeTitle)
+                        .bold()
+                        .foregroundColor(.white)
+                        .padding()
+                    
+                    Text(card.description)
+                        .font(.title3)
+                        .foregroundColor(.white)
+                        .padding()
+                    
+                    Spacer()
+                    
+                    Text("Guess My Future 🔮")
+                        .font(.title2)
+                        .bold()
+                        .foregroundColor(.white)
+                        .padding()
+                        .background(Color.white.opacity(0.3))
+                        .cornerRadius(60)
+                }
+                .padding(.bottom, 50)
             }
-            .padding(.bottom, 50)
             .frame(width: 300, height: 650)
-            .background(card.color)
             .cornerRadius(40)
             .padding()
             .shadow(color: Color.black.opacity(0.2), radius: 10, x: 0, y: 10) // Add shadow here
+        }
+    }
+}
+
+struct CardBackgroundView: View {
+    var card: Card
+
+    var body: some View {
+        GeometryReader { geometry in
+            card.color
+                .overlay(
+                    ForEach(0..<9) { _ in
+                        CardEmojiView(card: card)
+                            .font(.system(size: CGFloat(arc4random_uniform(50) + 50)))  // Random size
+                            .opacity(0.3)
+                            .rotationEffect(.degrees(Double(Int(arc4random_uniform(45)) - 20)))  // Random rotation
+                            .position(x: CGFloat(arc4random_uniform(UInt32(geometry.size.width))),
+                                      y: CGFloat(arc4random_uniform(UInt32(geometry.size.height*2/3))+UInt32(geometry.size.height/3)))
+                    }
+                )
+        }
+    }
+}
+
+struct CardEmojiView: View {
+    var card: Card
+
+    var body: some View {
+        if card.title == "Love" {
+            Text("❤️")
+        } else if card.title == "Finance" {
+            Text("💵")
+        } else if card.title == "Family" {
+            Text("👨‍👩‍👦")
+        } else if card.title == "Death" {
+            Text("💀")
+        } else {
+            Text("")
         }
     }
 }
@@ -49,6 +90,21 @@ struct Card_Previews: PreviewProvider {
         CardView(card: Card(title: "Love", description: "Seek insight into your romantic relationships and feelings. Love might be just a card away.", color: LinearGradient(gradient: Gradient(colors: [Color.pink, Color.red]), startPoint: .top, endPoint: .bottom)))
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
